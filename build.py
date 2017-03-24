be0c9ed63c9dc89a86163cd06dbee997a8894f69
Antoine Bolvy <antoine.bolvy@gmail.com>
2017-03-23 20:58:51 -0400

    Add pandoc filter for doxygen link generation (with specific syntax in md)

--- build.py
+++ build.py
@@ -170,6 +170,7 @@ class Build(object):
             '--filter=./pandoc-filters/pandoc-svg.py',
             '--filter=./pandoc-filters/pandoc-graphviz.py',
             '--filter=./pandoc-filters/pandoc-header-images-to-latex.py',
+            '--filter=./pandoc-filters/pandoc-generate-doxylinks.py',
         ]
 
         if 'header_shift' in file_config and file_config['header_shift']: # also check if not 0
---
+++ pandoc-filters/pandoc-generate-doxylinks.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+"""
+Pandoc filter to replace namespace in code inlines to short class names with links
+"""
+
+__author__ = "Antoine Bolvy"
+
+from panflute import run_filter, Image, Str, RawInline, CodeBlock, Code, Link
+import lxml.html as html
+import sys
+import re
+
+# inspired from http://stackoverflow.com/a/30490482/2367848
+def doxymangle(identifier):
+    """
+    Converts a string to doxygen url format
+    """
+    substitutions = {
+        '_': "__",
+        '-': "-",
+        ':': "_1",
+        '/': "_2",
+        '<': "_3",
+        '>': "_4",
+        '*': "_5",
+        '&': "_6",
+        '|': "_7",
+        '.': "_8",
+        '!': "_9",
+        ',': "_00",
+        ' ': "_01",
+        '{': "_02",
+        '}': "_03",
+        '?': "_04",
+        '^': "_05",
+        '%': "_06",
+        '(': "_07",
+        ')': "_08",
+        '+': "_09",
+        '=': "_0A",
+        '$': "_0B",
+        '\\': "_0C",
+    }
+    out = ''
+    for char in identifier:
+        if char in substitutions:
+            out += substitutions[char]
+        else:
+            out += char
+    return out
+
+
+def doxylink(elem, _):
+    """
+    Apply the doxylink
+    """
+    # We are only interested in "raw_html" elements
+    if not isinstance(elem, Link):
+        return
+
+    # https://regex101.com/r/ZS5Fu6/1
+    res = re.match(r'^\#(lug(?:\:\:[\w]+)+?)(\:\:[\w]+\(\))?$', elem.url)
+
+    if res:
+        elem.url = 'https://lugdunum3d.github.io/docs/class' + doxymangle(res.group(1)) + '.html'
+
+
+def main(doc=None):
+    """
+    Run the fitler
+    """
+    return run_filter(doxylink, doc=doc)
+
+if __name__ == "__main__":
+    main()
